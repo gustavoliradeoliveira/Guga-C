0915f22d57efb3fefb54824a09884f8e91ea0db9
gustavoliradeoliveira <135131490+gustavoliradeoliveira@users.noreply.github.com>
2025-08-08 21:45:28 -0300

    Update main.c

--- Gustavo - Aula C/Aula C/main.c
+++ Gustavo - Aula C/Aula C/main.c
@@ -103,8 +103,215 @@ int a, b, x, v, h, j;
     h = a+b;
     j = a-b;
 
-     printf("Multiplica��o : %d\n Divis�o: %d\n Soma: %d\n Subtra��o: %d\n", x,v,h,j);
+
+
+
+    #include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+/*
+    int num1;
+    int num2;
+    double media;
+printf("Informe o primeiro Numero:");
+    scanf("%d", &num1);
+
+
+    printf("Informe o segundo Numero:");
+        scanf("%d", &num2);
+
+        media = (num1 + num2) / 2;
+
+        printf("A media eh: %.2lf\n", media);
+
+*/
+
+/*
+Numero ao Quadrado
+    int num1;
+    int quadrado;
+    printf("Informe um Numero:");
+        scanf("%d", &num1);
+        quadrado = num1 * num1;
+        printf("O quadrado do Numero eh: %d\n", quadrado);
+*/
+/*Reajuste |Salarial
+
+    double salario, novoSalario, reajuste;
+
+    printf("Informe o Salario:");
+        scanf("%lf", &salario);
+            reajuste = salario/100;
+        novoSalario = salario + reajuste;
+    printf("Novo salario: %.2lf\n", novoSalario);
+*/
+
+/*
+Desconto
+double salario, novoSalario, reajuste;
+
+    printf("Informe o Salario:");
+        scanf("%lf", &salario);
+            reajuste = salario/100;
+        novoSalario = salario - reajuste;
+    printf("Novo salario: %.2lf\n", novoSalario);
+*/
+/*
+double preco, desconto, novoPreco;
+
+    printf("Informe o valor do Product:");
+        scanf("%lf", &preco);
+            Desconto = preco/100 ;
+        novoSalario = salario - reajuste;
+    printf("Novo salario: %.2lf\n", novoSalario);
+    */
+
+
+/*
+    char v1, v2, v3;
+    printf("Informe o Primeiro Caracter:");
+    scanf("%c", &v1);
+
+    printf("informe o segundo caracter:");
+    scanf(" %c", &v2);
+
+    printf("informe o terceio caracter:");
+    scanf(" %c", &v3);
+
+    printf("%c, %c, %c\n", v1, v2, v3);
+    */
+
+    /*
+    int a, b, c, x;
+    printf("Informe o Primeiro Caracter:");
+    scanf("%d", &a);
+
+    printf("informe o segundo caracter:");
+    scanf(" %d", &b);
+
+    printf("informe o terceio caracter:");
+    scanf(" %d", &c);
+
+    x = 2*((a - c)/8)-b*5;
+
+    printf("Valor de x: %d\n", x);
+*/
+/*
+int a, b, x, v, h, j;
+    printf("Informe o Primeiro Caracter:");
+    scanf("%d", &a);
+
+    printf("informe o segundo caracter:");
+    scanf(" %d", &b);
+
+    x = a*b;
+    v = a/b;
+    h = a+b;
+    j = a-b;
+
+     printf("Multiplicação : %d\n Divisão: %d\n Soma: %d\n Subtração: %d\n", x,v,h,j);
+
+*/
+/*
+int s, resultado;
+printf("escreva um Numero Inteiro");
+    scanf("%d", &s);
+    s+=1;
+    printf("Novo valor: %d\n", s);
+*/
+
+    
+    
+    
+    
+    
+    
+    
+    Aula dia 08/09/2025
+/*
+int raio, area, perimetro;
+double pi = 3,14;
+
+    // Solicitar o valor do raio ao usuário
+    printf("Digite o valor do raio do círculo: ");
+    scanf("%d", &raio);
+
+    // Calcular a área e o perímetro
+    area = pi * raio * raio;
+    perimetro = 2 * PI * raio;
+
+    // Exibir os resultados
+    printf("Área do círculo: %.2f\n", area);
+    printf("Perímetro do círculo: %.2f\n", perimetro);
+    return 0;
+    */
+
+
+
+/*
+    double raio;
+    printf("informe um valor para o raio para descobrimos o perimetro");
+    scanf("%lf",&raio);
+    raio = 2 * raio * 3,14;
+    printf("O valor do perimetro é %.2lf\n", raio );
+    */
+
+    /*
+    int idade;
+    printf("escreva sua idade:");
+    scanf("%d", &idade);
+    idade = idade * 365;
+    printf("seu tempo em dias vividos %d\n", idade);
+    */
+
+
+/*
+    double num, resultado;
+    printf("escreva Um Numero: ");
+    scanf("%lf", &num);
+    resultado = num*0.04;
+    printf("Esse numero representa 4 do seu numero: %.2lf\n", num);
+    */
+/*
+    float nota1, nota2, nota3, media;
+
+    printf("Digite as três notas do aluno:\n");
+    scanf("%f %f %f", &nota1, &nota2, &nota3);
+
+    media = (nota1*2 + nota2*3 + nota3*5) / 10;
+
+    printf("Média do aluno: %.2f\n", media);
+
+*/
+/*
+float distancia, tempo, velocidadeMedia;
+
+    printf("Digite a distância percorrida (em km): ");
+    scanf("%f", &distancia);
+
+    printf("Digite o tempo gasto (em horas): ");
+    scanf("%f", &tempo);
+
+    velocidadeMedia = distancia / tempo;
+
+    printf("Velocidade média: %.2f km/h\n", velocidadeMedia);
+*/
+/*
+float valor, imposto, pcDistribuidor;
+Printf("Informe o valor de Fabrica %fl", &valor);
+Scan
+*/
+
+int
+
+ return 0;
+
+}
+     printf("Multiplicação : %d\n Divisão: %d\n Soma: %d\n Subtração: %d\n", x,v,h,j);
 
 
     return 0;
 }
+
